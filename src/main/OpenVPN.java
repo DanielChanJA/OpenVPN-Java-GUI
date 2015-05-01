a43199dbcc49426c4f81c96d59c1f8c5db10c525
Daniel Chan <iam@danielchan.me>
2015-05-01 19:16:29 +0800

    Created blank methods, to be used later on.
    GUI.java created for the GUI for the program.
    UserPass.java to create the textfile in which openvpn will read the
    username and password from.

--- src/main/OpenVPN.java
+++ src/main/OpenVPN.java
@@ -1,22 +1,28 @@
+/*
+ * This class contains the methods for the connection. For now this class will contain methods that I will personally use. 
+ * But users can adapt it to their settings. 
+ * 
+ */
+
+
+
 package main;
 
 import java.io.*;
 
 
 public class OpenVPN {
-
-	public static String errorConnecting, userpassIncorrect, tryAgain;
+	
+	public String errorConnecting, userpassIncorrect, tryAgain, openvpn_path;
 	
 	
-	public static void main (String[] args) {
-		
-		
-		//Instance the OpenVPNs but don't execute the commands unless told to.
-		Runtime OVPN_HK1 = Runtime.getRuntime();
-		Runtime OVPN_LAX1 = Runtime.getRuntime();
-		Runtime OVPN_LV1 = Runtime.getRuntime();
-		Runtime OVPN_NY1 = Runtime.getRuntime();
-		
+	//Instance the OpenVPNs but don't execute the commands unless told to.
+	Runtime OVPN_HK1 = Runtime.getRuntime();
+	static Runtime OVPN_LAX1 = Runtime.getRuntime();
+	static Runtime OVPN_LV1 = Runtime.getRuntime();
+	static Runtime OVPN_NY1 = Runtime.getRuntime();
+	
+	public void bootUP() {
 		
 		errorConnecting = "Unable to connect to servers at this time. Please try again later.";
 		userpassIncorrect = "You have entered an invalid username or password.";
@@ -24,16 +30,56 @@ public class OpenVPN {
 		
 		
 		
+	}
+	
+	
+	public void openvpn_HK1() {
+		
+		
+		//Temporarily added, will link up with GUI soon. Do not touch yet!
 		try {
-			Process pr_OVPN = OVPN_HK1.exec("openvpn");
+			Process oHK1 = OVPN_HK1.exec("openvpn hk1-scrambled.ovpn");
 		} catch (IOException e) {
 			System.out.println("Unable to find the files.");
 			e.printStackTrace();
 		}
 		
 		
+	}
+	
+	
+	public void openvpn_LAX1() {
+	
+		try {
+			Process oLAX1 = OVPN_LAX1.exec("openvpn lax1-scrambled.ovpn");
+		} catch (IOException e) {
+			System.out.println("Unable to find the files.");
+			e.printStackTrace();
+		}
 		
 		
+	}
+	
+	
+	public void openvpn_LV1() {
+		
+		try {
+			Process oLV1 = OVPN_LV1.exec("openvpn lax1-scrambled.ovpn");
+		} catch (IOException e) {
+			System.out.println("Unable to find the files.");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void openvpn_NY1() {
+		
+		try {
+			Process oNY1 = OVPN_NY1.exec("openvpn lax1-scrambled.ovpn");
+		} catch (IOException e) {
+			System.out.println("Unable to find the files.");
+			e.printStackTrace();
+		}
 		
 	}
 	
---
+++ src/main/UserPass.java
@@ -0,0 +1,30 @@
+/*
+ * This class is designed for the creation of the username and password file for OpenVPN to read from.
+ * It's very simple and does not contain any security for the plaintext file. Not sure if theres anyway to make it anymore secure.
+ * 
+ */
+
+
+package main;
+
+public class UserPass {
+
+	
+	
+	
+	
+	public void createUsername() {
+		
+		
+	}
+	
+	public void createPassword() {
+		
+		
+	}
+	
+	
+	
+	
+	
+}
